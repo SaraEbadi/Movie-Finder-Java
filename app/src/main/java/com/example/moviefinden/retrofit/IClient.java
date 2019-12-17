b4f8c831a5b3138373ed815d2708a037dfdb7995
saraebadi <ebadi.sara227@gmail.com>
2019-12-17 19:15:02 +0330

    configuration gradle

--- app/src/main/java/com/example/moviefinden/retrofit/IClient.java
+++ app/src/main/java/com/example/moviefinden/retrofit/IClient.java
@@ -3,6 +3,7 @@ package com.example.moviefinden.retrofit;
 import com.example.moviefinden.models.DetailsModel;
 import com.example.moviefinden.models.MovieModel;
 
+import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -11,7 +12,7 @@ import retrofit2.http.Query;
 public interface IClient {
 
     @GET("search/movie")
-    Call<MovieModel> getSearchMovie(
+    Observable<MovieModel> getSearchMovie(
             @Query("api_key") String apiKey,
             @Query("query") String query
     );
